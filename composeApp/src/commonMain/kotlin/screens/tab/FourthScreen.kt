bc2fe4dd00f444e90a6d7fa5563eb4b700ad0e73
Michał Konkel <Michal.Konkel@velobank.pl>
2024-07-17 08:54:48 +0200

    updating images

--- composeApp/src/commonMain/kotlin/screens/tab/FourthScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/tab/FourthScreen.kt
@@ -2,11 +2,14 @@ package screens.tab
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import navigation.tab.FourthScreenComponent
 
 @Composable
@@ -20,6 +23,7 @@ fun FourthScreen(
     ) {
         Text("TAB TWO")
         Text(component.text)
+        Spacer(modifier = Modifier.height(16.dp))
         Text("COUNTDOWN: ${component.countDownText.value}")
     }
 }
